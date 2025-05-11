f30be0c04951d1123eb2510789966c7aa82e90be
luongtrandao@gmail.com <luongtrandao@gmail.com>
2025-05-11 17:12:25 +0700

    init commit

--- src/main/java/com/mycompany/hms/HMS.java
+++ src/main/java/com/mycompany/hms/HMS.java
@@ -21,12 +21,18 @@ public class HMS {
         plist.Insert(p3);
         
         Doctor d1 = new Doctor("11","TRUNGKI","14121451","",100);
-        Doctor d2 = new Doctor("22","TRUNGKI","14121451","",200);
-        Doctor d3 = new Doctor("33","TRUNGKI","14121451","",300);
+        Doctor d2 = new Doctor("22","WAD","14121451","",200);
+        Doctor d3 = new Doctor("33","DWKI","14121451","",300);
         
+        DoctorsList dl = new DoctorsList();
+        dl.Insert(d1);
+        dl.Insert(d2);
+        dl.Insert(d3);
+        dl.AllDoctorInfo();
         
-        System.out.println(plist.searchByID("11").getName());
+                
+        System.out.println(dl.searchByID("11").getName());
         
-        System.out.println(plist.searchByContact("1412511").getName());
+        System.out.println(dl.searchByContact("14121451").getName());
     }
 }
